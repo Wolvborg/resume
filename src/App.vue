43bd4ff361b16fc9720ea86b39d204a4113cf5c3
Wolvborg <binnu24668@gmail.com>
2024-04-20 12:39:38 +0530

    Added tailwind components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <script setup></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <main class="flex">
+    <div class="bg-primary-fade w-1/3 h-full p-4">
+      <div class="text-center mb-4">
+        <p class="title">Abhishek Saini</p>
+        <p class="sub-title">(Senior Frontend Web Developer)</p>
+      </div>
+
+      <div class="border-2 p-2 border-primary rounded text-base flex flex-col gap-1.5">
+        <a href="https://maps.app.goo.gl/2GLnkxiw2hAequ8F7">Delhi, India</a>
+        <a href="mailto:abhishek.saini.0607@gmail.com">abhishek.saini.0607@gmail.com</a>
+        <a href="https://www.linkedin.com/in/abhishek-saini-0607/">abhishek-saini-0607</a>
+        <a href="https://github.com/Wolvborg">Wolvborg</a>
+        <a href="tel:+917982067969">+91-7982067969</a>
+      </div>
+    </div>
+    <div class="h-full"></div>
+  </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      current: 'current',
+      primary: '#8BC34A',
+      'primary-fade': '#EEF6E4',
+      black: '#000',
+      white: '#fff',
+      grey: '#808080',
+    },
     extend: {},
   },
   plugins: [],
